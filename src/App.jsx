d9319941dd4be342408fd04698a45b0e84d571bb
Thalia999 <thaliac000@gmail.com>
2026-04-06 12:13:16 -0400

    moved header, added const description and fucntion. added the import option

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
-function App() {
+import reactImg from './assets/react-core-concepts.png';
+
+function Header() {
+  const reactDescriptions =[' Fundamental', 'Crucial', 'Core']
+  function getRandomInt (max) {
+    return Math.floor(Math.random() * (max+1));
+  }
+  const description = reactDescriptions[getRandomInt(2)]
   return (
-    <div>
-      <header>
+    <header>
         <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
         <h1>React Essentials</h1>
         <p>
-          Fundamental React concepts you will need for almost any app you are
+          {description}React concepts you will need for almost any app you are
           going to build!
         </p>
       </header>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
       <main>
         <h2>Time to get started!</h2>
       </main>
